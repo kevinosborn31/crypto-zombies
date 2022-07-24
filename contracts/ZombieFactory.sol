1f504a1b806d656dd397dfde7fcb6a23ec7ea00c
kevinosbornjb <kevinosborn31@gmail.com>
2022-07-24 10:17:13 +1000

    Contract completed

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -1,6 +1,8 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
+
     uint256 dnaDigits = 16;
     uint256 dnaModulus = 10**dnaDigits;
 
@@ -11,7 +13,22 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function createZombie(string memory _name, uint256 _dna) public {
-        zombies.push(Zombie(_name, _dna));
+    function _createZombie(string memory _name, uint256 _dna) private {
+        uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 }
